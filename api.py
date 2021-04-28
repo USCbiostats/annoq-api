f39bb2a31042f68d22b2db4fc3e9e01bfd32d72c
tmushayahama <tmushayahama@gmail.com>
2021-04-28 09:41:34 -0700

    update port

--- api.py
+++ api.py
@@ -108,5 +108,5 @@ def proxy(path):
         return response
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0',debug = True,port=3404)
+    app.run(host = '0.0.0.0',debug = True,port=3403)
 
